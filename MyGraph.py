7ad2662e0027ee80b3a13a6f75c2857be95a5ce7
Vorbius <blake_whaley1@balor.edu>
2023-08-28 14:31:40 -0500

    initial commit

---
+++ MyGraph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
+
+#go to the  ... at next to the search and create a new terminal. Then type py -3 -m venv introvenv. In explorer, it sould be available
+#now you can switch interpreteres. 
+#if you want to install a library a library, the code is: pip install (name of library)
